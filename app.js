1a3bc8f3fe2a13bc32993bc17fc2e7db1b3a9dfa
sergioadll <sergio.adll@hotmail.com>
2021-03-19 19:47:28 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// Do not modify this code below.
+const ana= {
+    name: 'Ana',
+    age: 55,
+    children: ['María']
+};
+const jose= {
+    name: 'José',
+    age: 65,
+    children: ['Samuel', 'Yolanda']
+};
+const juan = {
+    name: 'Juan',
+    age: 57,
+    children: ['Pablo', 'Petra', 'Marina']
+};
+const parents = [ana, jose, juan];
+
+console.log(parents);
+/*
+1. Imprimir La lista de clase.
+2. Imprimir el nombre del hijos con padres jubilados (mayores de 65 años)
+
+tips:
+***** Buscar en google cómo recorrer objetos y arrays en javaScript ****
+
+1.    recorrer array -> acceder a la "key" children -> recorrer array -> imprimir
+2.    utilizar condicionales (if)
+
+*/
+// Start your code from here:
+
