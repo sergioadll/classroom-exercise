d002428899cc48b505d890177cee4e7f47bc65b4
sergioadll <sergio.adll@hotmail.com>
2021-03-19 20:43:33 +0000

    improved explanation

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-// Do not modify this code below.
+/*
+EJERCICIO:
+1. Imprimir La lista de clase.
+2. Imprimir el nombre del hij@s con padres jubilados (mayores de 65 años)
+
+tips:
+***** Buscar en google cómo recorrer objetos y arrays en javaScript o ver el video explicativo de la clase del 19/03****
+
+Empezar por crear una función que se encargue retornar la lista de clase (nombres de los hij@s)
+Recordar llamar a la función, pasándole el array "parents"
+
+1.    recorrer array -> acceder a la "key" o "clave" children -> recorrer array -> imprimir Nombre del hijo
+2.    utilizar condicionales (if)
+
+**** Ejecutar el código escribiendo en el terminal: node app.js ****
+
+*/
+
+// Do not modify this code.
 const ana= {
     name: 'Ana',
     age: 55,
@@ -16,18 +34,6 @@ const juan = {
 };
 const parents = [ana, jose, juan];
 
-console.log(parents);
-/*
-1. Imprimir La lista de clase.
-2. Imprimir el nombre del hijos con padres jubilados (mayores de 65 años)
+// Start your code below this line:
 
-tips:
-
-***** Buscar en google cómo recorrer objetos y arrays en javaScript o ver el video explicativo de la clase del 19/03****
-
-1.    recorrer array -> acceder a la "key" o "clave" children -> recorrer array -> imprimir Nombre del hijo
-2.    utilizar condicionales (if)
-
-*/
-// Start your code from here:
-
+console.log(parents); // you can delete this if you want.
